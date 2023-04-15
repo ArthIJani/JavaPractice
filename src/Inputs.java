b61e8e742753d61e6409db0ba287c3a410e3b487
ArthIJani <arthjani20@gnu.ac.in>
2023-04-15 16:52:20 +0530

    Initial commit

--- src/Inputs.java
+++ src/Inputs.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-public class Main {
+
+public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println(input.nextLine());
 
     }
 }
