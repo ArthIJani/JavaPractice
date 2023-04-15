dee33a3e4710c032f865eb236fcd9efb5a0c14bf
ArthIJani <arthjani20@gnu.ac.in>
2023-04-15 17:34:51 +0530

    Initial commit

--- src/Inputs.java
+++ src/Inputs.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        System.out.print("Please Enter Some Input: ");
+        int rollno = input.nextInt();
+        System.out.println("Your roll number is "+ rollno);
     }
 }
